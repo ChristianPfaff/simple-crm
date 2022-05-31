4c4506458f55f5ee4c30c775fe26c1706b1098d6
Christian Pfaff <madadaskar@googlemail.com>
2022-05-31 21:31:43 +0200

    reworked 2022_05_31

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/models/user.class';
+import { DialogEditAddressComponent } from '../dialog-edit-address/dialog-edit-address.component';
+import { DialogEditUserComponent } from '../dialog-edit-user/dialog-edit-user.component';
+
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +13,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.scss']
 })
 export class UserDetailComponent implements OnInit {
+  userId:any = '';
+  user: User = new User();
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private firestore: AngularFirestore, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( paramMap => {
+      this.userId = paramMap.get('id');
+      console.log('GOT ID', this.userId);
+      this.getUser();        
+    })
+  }   
+  
+  getUser() {
+   this.firestore.collection('users').doc(this.userId).valueChanges().subscribe((user: any) => {
+    this.user = new User(user);    
+    console.log('Retrieved user',this.user);    
+   });       
   }
 
+  editMenu(){
+    this.dialog.open(DialogEditAddressComponent);
+  }
+
+  editUserDetail(){
+    this.dialog.open(DialogEditUserComponent);
+  }
+    
 }
+
